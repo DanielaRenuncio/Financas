2035ace99ba19c128166fdb27a6ad06bbf6b505c
Daniela Renuncio <danielarenuncio@gmail.com>
2020-01-20 16:17:22 -0300

    Iniciando cadastro de gastos[Fix #7]

---
+++ src/Controller/MyExpensesController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Code\Controller;
+
+use Code\View\View;
+
+class MyExpensesController 
+{
+    public function index()
+    {
+
+    }
+
+    public function new()
+    {
+        $view = new View('expenses/new.phtml');
+
+        return $view->render();
+    }
+}
